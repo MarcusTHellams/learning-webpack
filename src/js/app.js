59b51e212d5aea7d293f2d63bca11327d9336453
CACI\marcus.hellam <marcus.hellam.caci.com>
2017-04-14 17:13:58 -0400

    getting ready for SASS lesson

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,26 @@ import '../css/input-elements.css';
 import { updateSecretParagraph } from './paragraph';
 import { updatesecretButton } from './button';
 import { secretButton } from './dom-loader';
+import { RandomGenerator } from './random-generator';
 var showSecret = false;
+
+const outputParagraph = document.querySelector('#outputParagraph');
+
+const outputRandomInt = () => {
+    outputParagraph.textContent = RandomGenerator.randomInteger();
+};
+
+
+const outputRandomRange = () => {
+    outputParagraph.textContent = RandomGenerator.randomRange(1, 500);
+};
+
+const buttonRndInt = document.querySelector('#randomInt');
+const buttonRndRange = document.querySelector('#randomRange');
+
+buttonRndInt.addEventListener('click', outputRandomInt);
+buttonRndRange.addEventListener('click', outputRandomRange);
+
 
 secretButton.addEventListener('click', toggleSecretState);
 updateSecretParagraph(showSecret);
@@ -13,3 +32,4 @@ function toggleSecretState() {
     updateSecretParagraph(showSecret);
     updatesecretButton(showSecret);
 }
+
